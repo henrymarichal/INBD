a94391a61a0846ac51a1a1addd1cf1ac77654ff9
henry <hmarichal93@gmail.com>
2024-02-05 18:46:18 -0300

    mend

--- cluster.sh
+++ cluster.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #SBATCH --job-name=inbd
-#SBATCH --ntasks=16
-#SBATCH --mem=40G
+#SBATCH --ntasks=8
+#SBATCH --mem=80G
 #SBATCH --time=04:00:00
 #SBATCH --mail-type=ALL
 #SBATCH --tmp=100G
