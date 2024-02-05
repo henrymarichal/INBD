d613aa5341a5ededdf0138393a19d98c2f7632f7
henry <hmarichal93@gmail.com>
2024-02-05 19:17:49 -0300

    repeat training from paper

--- cluster.sh
+++ cluster.sh
@@ -24,6 +24,7 @@ conda activate inbd_gpu
 LOCAL_NODE_DIR=/scratch/henry.marichal/
 #HOME_DATASET_DIR=~/repos/INBD/dataset/EH
 HOME_DATASET_DIR=~/datasets/inbd_4
+HOME_DATASET_DIR=~/datasets/INBD/EH
 HOME_SEGMENTATION_MODEL=~/resultados/inbd_pinus_taeda_20240203_011323/model/2024-02-03_01h13m37s_segmentation_300e_x4_/model.pt.zip
 HOME_RESULTADOS_DIR=~/resultados/inbd_pinus_taeda_$(date +'%Y%m%d_%H%M%S')
 HOME_RESULTADOS_MODEL_DIR=$HOME_RESULTADOS_DIR/model
@@ -66,9 +67,9 @@ check_command_result mkdir -p $HOME_RESULTADOS_DIR
 # Run the program
 
 cd ~/repos/INBD/
-#python main.py train segmentation $HOME_DATASET_DIR/train_inputimages.txt $HOME_DATASET_DIR/train_annotations.txt --output $HOME_RESULTADOS_MODEL_DIR --epochs 300 > "$stdout_file" 2> "$stderr_file"
+python main.py train segmentation $HOME_DATASET_DIR/train_inputimages.txt $HOME_DATASET_DIR/train_annotations.txt --output $HOME_RESULTADOS_MODEL_DIR --epochs 300 > "$stdout_file" 2> "$stderr_file"
 #next, train the inbd network
-python main.py train INBD  $HOME_DATASET_DIR/train_inputimages.txt   $HOME_DATASET_DIR/train_annotations.txt   --segmentationmodel=$HOME_SEGMENTATION_MODEL  --output $HOME_RESULTADOS_MODEL_DIR > "$stdout_file" 2> "$stderr_file" #adjust path
+#python main.py train INBD  $HOME_DATASET_DIR/train_inputimages.txt   $HOME_DATASET_DIR/train_annotations.txt   --segmentationmodel=$HOME_SEGMENTATION_MODEL  --output $HOME_RESULTADOS_MODEL_DIR > "$stdout_file" 2> "$stderr_file" #adjust path
 
 # -------------------------------------------------------
 #copy results to HOME
