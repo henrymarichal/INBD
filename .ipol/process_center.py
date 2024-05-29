e21277f2680db7ef468854cf80ea2a877c49ac46
henry <hmarichal93@gmail.com>
2024-05-29 22:00:09 +0200

    test final

--- .ipol/process_center.py
+++ .ipol/process_center.py
@@ -3,7 +3,7 @@ import argparse
 import cv2
 from shapely.geometry import Polygon
 from pathlib import Path
-def mannually(filename, img_path):
+def mannually(filename, img_path, output_img):
     import os
     #open txt with pandas
     f = open(filename, "r")
@@ -20,8 +20,8 @@ def mannually(filename, img_path):
     mask = np.zeros(img.shape[:2], dtype=np.uint8)
     cv2.fillPoly(mask, np.array([poly], dtype=np.int32), 255)
     #save mask
-    mask_filename = str(Path(img_path).parent / "mask.png")
-    cv2.imwrite(mask_filename, mask)
+    cv2.imwrite(output_img, mask)
+    print(output_img)
 
     return
 
@@ -29,9 +29,10 @@ if __name__=="__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("--input_poly", type=str, required=True)
     parser.add_argument("--input_img", type=str, required=True)
+    parser.add_argument("--output_img", type=str, required=True)
 
     args = parser.parse_args()
 
-    mannually(args.input_poly, args.input_img)
+    mannually(args.input_poly, args.input_img, args.output_img)
 
 
--- .ipol/run.sh
+++ .ipol/run.sh
@@ -3,16 +3,19 @@ set -e
 
 # Read input parameters
 input_0=$1
-BIN=$2
-HOME=$3
+input_1=$2
+output_0=$3
+BIN=$4
+HOME=$5
 
 # Extract center from mask
+python $BIN/.ipol/process_center.py --input_poly $input_1 --input_img $input_0 --output_img $output_0
 
-if [ -s inpainting_data_0.txt ]; then
+#if [ -s inpainting_data_0.txt ]; then
   # File is not empty
-  python $BIN/.ipol/process_center.py --input_poly inpainting_data_0.txt --input_img $input_0
+#  python $BIN/.ipol/process_center.py --input_poly inpainting_data_0.txt --input_img $input_0 --output_img $output_0
   #rm inpainting_data_0.txt
-fi
+#fi
 #else
 #  # File is  empty
 #  $ANT_CENTER_DETECTOR/build/AntColonyPith --animated=false --input $input_0
@@ -28,5 +31,5 @@ fi
 #python $BIN/main.py --detection_path $input_0 --disk_name $disk_name --th $threshold --output_dir $HOME
 
 #python $BIN/main.py --input $input --cx $Cx --cy $Cy --root $BIN --output_dir ./  --th_high $th_high --th_low $th_low --hsize $hsize --wsize $wsize --sigma $sigma --save_imgs 1
-python $BIN/main.py inference $BIN/models/model.pt.zip $input_0 mask.png --output $HOME/output/
+python $BIN/main.py inference $BIN/models/model.pt.zip $input_0 $output_0 --output $HOME/output/
 
