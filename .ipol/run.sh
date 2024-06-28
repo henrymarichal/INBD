38aeb210cbcd30f253661c8cec861547414d55b3
henry <hmarichal93@gmail.com>
2024-06-28 13:53:17 -0300

    add model selection

--- .ipol/run.sh
+++ .ipol/run.sh
@@ -9,9 +9,10 @@ BIN=$4
 HOME=$5
 wsize=$6
 hsize=$7
+model=$8
 
 # Extract center from mask
 python $BIN/.ipol/preprocessing.py --input_poly $input_1 --input_img $input_0 --mask_path $output_0 --hsize $hsize --wsize $wsize
 
-python $BIN/main.py inference $BIN/models/model.pt.zip $input_0 $output_0 --output $HOME/output/
+python $BIN/main.py inference $BIN/models/$model $input_0 $output_0 --output $HOME/output/
 
